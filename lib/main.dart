d55dd0747ba99906b71c66c1b8279a90466e4f75
fuser <sahil.dogstar@gmail.com>
2019-10-03 21:42:52 +0530

    Finally Done.

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,83 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.black ,
-      appBar: AppBar(
-        title: Text(
-          'Movies Poster',
-          style: TextStyle(color: Colors.black),
-        ),
-        backgroundColor: Colors.yellow,
-      ),
-      body: Container(),// Use stateful widget you created here
-    ),
-  ));
+  runApp(
+      MaterialApp(
+        home: Movies(),
+  ),
+  );
 }
+String listOfMovies = MoviesList.moviesJsonList;
+
+class Movies extends StatefulWidget {
+  @override
+  _MoviesState createState() => _MoviesState();
+}
+
+class _MoviesState extends State<Movies> {
+
+
+  List movies = jsonDecode(listOfMovies);
+  String nextMovie = "https://raw.githubusercontent.com/android10/Sample-Data/master/Android-CleanArchitecture-Kotlin/posters/038001.jpg";
+  int movieNumber = 0;
+
+  void viewNextMovie(){
+    setState(() {
+      movieNumber = movieNumber + 1;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+
+
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text(
+            'Movies Poster',
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.yellow,
+        ),
+        body: Container(
+          color: Colors.black87,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    flex: 10,
+                    child: Image(
+//                height: 300,
+//                width: 300,
+                      image: NetworkImage(movies[movieNumber]["poster"]),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Card(
+                      color: Colors.yellow,
+                      child: FlatButton(
+                        child: Text('Next Movie'),
+                        onPressed: viewNextMovie,
+    ),
+            ),
+          ),
+            ],
+          ),
+        ),
+//        get you created here
+      ),
+        ),
+        ),
+    );
+  }
+}
+
+
 
 // You are given json string of movies list (see file movies.dart)
 String moviesList = MoviesList.moviesJsonList;
