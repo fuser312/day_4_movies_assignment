920efe514d11de835aa99b5792592f599b3f1d92
fuser <sahil.dogstar@gmail.com>
2019-10-04 16:51:43 +0530

    Carousel Completed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:day_4_movies_assignment/movies.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() {
   runApp(
@@ -22,15 +23,20 @@ class _MoviesState extends State<Movies> {
   List movies = jsonDecode(listOfMovies);
   String nextMovie = "https://raw.githubusercontent.com/android10/Sample-Data/master/Android-CleanArchitecture-Kotlin/posters/038001.jpg";
   int movieNumber = 0;
+  List newListOfMovies = [];
 
   void viewNextMovie(){
     setState(() {
       movieNumber = movieNumber + 1;
     });
+
   }
   @override
   Widget build(BuildContext context) {
-
+    for (int i = 0; i < movies.length; i++){
+      newListOfMovies.add(movies[i]["poster"]);
+    }
+    print(newListOfMovies);
 
     return SafeArea(
       child: Scaffold(
@@ -42,37 +48,40 @@ class _MoviesState extends State<Movies> {
           ),
           backgroundColor: Colors.yellow,
         ),
-        body: Container(
-          color: Colors.black87,
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Expanded(
-                    flex: 88,
-                    child: Image(
-//                height: 300,
-//                width: 300,
-                      image: NetworkImage(movies[movieNumber]["poster"]),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 8,
-                    child: Card(
-                      color: Colors.yellow,
-                      child: FlatButton(
-                        child: Text('Next Movie'),
-                        onPressed: viewNextMovie,
-    ),
+        body: Center(
+          child: Container(
+            color: Colors.black87,
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CarouselSlider(
+                      height: 400.0,
+                      autoPlay: true,
+                      reverse: true,
+                      items: newListOfMovies.map((i) {
+                        return Builder(
+                          builder: (BuildContext context) {
+                            return Container(
+                              width: MediaQuery.of(context).size.width,
+                              margin: EdgeInsets.symmetric(horizontal: 5.0),
+                              child:( Image(image: NetworkImage(i),
+                                fit: BoxFit.cover,
+                              )
+                              ),
+                            );
+                          },
+                        );
+                      }).toList(),
+                    )
+              ],
             ),
           ),
-            ],
-          ),
-        ),
 //        get you created here
       ),
+          ),
         ),
         ),
     );
